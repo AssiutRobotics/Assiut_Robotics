26333b6a404dbac782ec76b34f3ad7c2552ad4f6
mazen magdy <69149378+Mazen-magdy@users.noreply.github.com>
2024-09-16 00:01:40 +0300

    Update Blog Page
    Note : It isn't ready to use

--- views/blog/script.js
+++ views/blog/script.js
@@ -1,34 +1,34 @@
-function readMoreShow() {
-    $(document).ready(function () {
-        var contents = $(".text-content");
-        var maxLength = 200;
+// function readMoreShow() {
+//     $(document).ready(function () {
+//         var contents = $(".text-content");
+//         var maxLength = 200;
 
-        contents.each(function () {
-            var content = $(this);
-            var text = content.text();
-            var fullText = text; // Store the full text
+//         contents.each(function () {
+//             var content = $(this);
+//             var text = content.text();
+//             var fullText = text; // Store the full text
 
-            if (text.length > maxLength) {
-                var truncatedText = text.substr(0, maxLength) + "...";
-                content.addClass("truncated").text(truncatedText);
+//             if (text.length > maxLength) {
+//                 var truncatedText = text.substr(0, maxLength) + "...";
+//                 content.addClass("truncated").text(truncatedText);
 
-                // Add toggle link after each truncated content
-                content.after('<a href="#" class="toggle-content show">Show more</a>');
-            }
+//                 // Add toggle link after each truncated content
+//                 content.after('<a href="#" class="toggle-content show">Show more</a>');
+//             }
 
-            // Delegate click event to dynamically added .toggle-content links
-            $(document).on("click", ".toggle-content", function (event) {
-                event.preventDefault();
-                var link = $(this);
-                var content = link.prev(".text-content");
-                var text = content.hasClass("truncated") ? fullText : fullText.substr(0, maxLength) + "...";
+//             // Delegate click event to dynamically added .toggle-content links
+//             $(document).on("click", ".toggle-content", function (event) {
+//                 event.preventDefault();
+//                 var link = $(this);
+//                 var content = link.prev(".text-content");
+//                 var text = content.hasClass("truncated") ? fullText : fullText.substr(0, maxLength) + "...";
 
-                content.toggleClass("truncated").text(text);
-                link.text(content.hasClass("truncated") ? "Show more" : "Show less");
-            });
-        });
-    });
-}
+//                 content.toggleClass("truncated").text(text);
+//                 link.text(content.hasClass("truncated") ? "Show more" : "Show less");
+//             });
+//         });
+//     });
+// }
 
 function loadBlogs() {
     let blogsContainer = document.querySelector(".unPinnedBlogs");
@@ -77,4 +77,4 @@ try {
     console.error(error);
 }
 
-readMoreShow();
+// readMoreShow();
