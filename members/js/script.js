399afc8a3d6ca3ece0816f51fd425872b4523466
mazen magdy <69149378+Mazen-magdy@users.noreply.github.com>
2024-10-21 18:21:01 +0300

    primary update

--- members/js/script.js
+++ members/js/script.js
@@ -8,28 +8,63 @@ const border = document.querySelector('.vline');
 var membersData = [];
 const container = document.getElementsByClassName(".members");
 
-const getMembersData = async () => {
-    const response = await fetch("../../members/getAllMembers")
-    if (response.ok) {
-        const res = await response.json()
-        membersData = res.data;
-        console.log(membersData);
 
-        membersData.forEach(element => {
-            container.innerHTML += `
-            <div class="component box">
-            <img src="../../uploads/${element.image}" alt="">
-            <div class="name"> ${element.title}</div>
-
-        </div>`
-        });
-
-    } else {
-        console.log(response);
+//fetching data from the api
+async function fetchingData() {
+    try{
+    let response = await fetch('https://assiutrobotics-production.up.railway.app/members/getAllMembers');
+    let data = await response.json();
+    return data.data.members;
+    }
+    catch(error){
+        console.log(error);
     }
 }
 
-getMembersData();
+// testing the data
+const testing = async () => {
+
+    membersData = await fetchingData();
+    console.log(membersData);
+
+
+    function searchFor(Committee, data) {
+        for (let i = 0; i < data.length; i++) {
+            
+            if (data[i].committee === Committee && data[i].role == /*For testing we will use the number*/ 2 /*'head'*/) {
+                console.log(data[i].avatar);
+                return data[i].avatar;
+            }
+        }
+    }
+    
+    function createMembers() {
+        let data = membersData;
+        let teamLeaderPicture = document.getElementById('PTeamLeader')
+        // let pictureLeader = searchFor('team', data)
+        let commitiesHeads = document.getElementById('CSlider') // this will contain the committees taps 
+        // Create the taps  // HR PR    marketing AC media web 
+        // solve the problem that the styles doesn't work
+        let arrOfCommities = ['HR', 'PR', 'marketing', 'AC', 'media', 'web']
+        for (let i = 0; i < 6; i++) {
+            let pciture = searchFor(arrOfCommities[i], data)
+            let content = `
+            <div class="col-2">
+              <div class="circle">
+                <img src="${pciture}" class="pic">
+              </div>
+              <h3>${arrOfCommities[i]}</h3>
+            </div>`
+            
+            commitiesHeads.innerHTML += content
+        }
+    }
+    createMembers()
+}
+
+testing();
+
+
 
 //clicking event for board members
 board.forEach(e => {
