f24e88c484d2a3261a46008ebbb9993de89e461e
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2024-10-22 00:08:21 +0300

    udpate

--- members/js/script.js
+++ members/js/script.js
@@ -94,28 +94,3 @@ const members = document.querySelectorAll('.circle2');
 //     })
 // });
 
-$('.slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 4,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 4
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
-});
